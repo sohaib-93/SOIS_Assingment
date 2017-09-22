ec82f1a261764cbee4978f5b8dc4fbf4953fa9e7
sohaib-93 <32169152+sohaib-93@users.noreply.github.com>
2017-09-22 19:52:31 +0530

    Delete Program6.py

--- Embedded_Linux/Part_B/Program6.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''Embedded Linux Assignment - Part B
-Program 6: Implement a python code to find the kernel version.
-'''
-
-
-import platform
-
-print("The Kernel version is",platform.uname()[2])
